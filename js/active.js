6eab1e00af9744c719e807af5d8f2fdc41df3678
Groverjun <1250901900@qq.com>
2017-10-11 16:24:17 +0800

    add

--- js/active.js
+++ js/active.js
@@ -2,7 +2,8 @@ $("#C_left a").click(function(){
 			localStorage.setItem("index",$(this).index())
 		})
 		window.onload=function(){
-			var index=localStorage.getItem("index")
+			
+			var index=localStorage.getItem("index")||0
 			console.log(index)
 			$("#C_left a").eq(index).addClass("act").siblings().removeClass("act")
 			$("#C_left a").eq(index).find("em").addClass("janAct").parent("a").siblings().find("em").removeClass("janAct")
